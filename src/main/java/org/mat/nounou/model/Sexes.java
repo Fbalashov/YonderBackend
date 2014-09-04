8058e8856007ae015dcfd878476c9169d083b161
Fuad F. Balashov <fbalashov@gmail.com>
2014-09-04 10:26:22 -0400

    removed decrypt test cases and fixed anotations in sexes model class

--- src/main/java/org/mat/nounou/model/Sexes.java
+++ src/main/java/org/mat/nounou/model/Sexes.java
@@ -1,19 +1,15 @@
 package org.mat.nounou.model;
 
-import java.util.Collection;
-
 import javax.persistence.Basic;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
 import javax.xml.bind.annotation.XmlRootElement;
-import javax.xml.bind.annotation.XmlTransient;
+
+import com.sun.istack.NotNull;
 
 @Entity
 @Table(name = "sexes")
@@ -26,12 +22,10 @@ public class Sexes {
     @Id
     @Basic(optional = false)
     @NotNull
-    @Size(min = 1, max = 1)
     @Column(name = "symbol")
     private String symbol;
     @Basic(optional = false)
     @NotNull
-    @Size(min = 1, max = 15)
     @Column(name = "name")
     private String name;
 
